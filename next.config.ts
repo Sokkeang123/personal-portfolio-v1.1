f2b1f1ebc71448a094ffaaf0406e25abf1537182
Hi i'm sokkeang <sokkeangsieb27225@gmail.com>
2026-08-02 17:01:51 +0700

    Configure github page export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/personal-portfolio-v1.1", // Add your repo name here
 };
 
 export default nextConfig;
