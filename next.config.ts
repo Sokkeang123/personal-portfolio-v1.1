b617b7aa572ffcb29ff6cda1f2e04ebffe1f0406
Hi i'm sokkeang <sokkeangsieb27225@gmail.com>
2026-08-02 17:35:43 +0700

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,11 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: "/personal-portfolio-v1.1", // Add your repo name here
+  basePath: "/personal-portfolio-v1.1",
+  assetPrefix: "/personal-portfolio-v1.1/",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
